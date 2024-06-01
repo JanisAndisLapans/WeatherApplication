2eec6f161e0225082b6a8e73d9c5959de2593a6e
Gustavs Jaundžeikars <gj21029@edu.lu.lv>
2024-06-01 10:23:05 +0300

    Working inital fetching

--- src/WeatherAppGUI/Location.java
+++ src/WeatherAppGUI/Location.java
@@ -5,13 +5,25 @@ public class Location {
 	private String name;
 	private double latitude;
 	private double longitude;
+	private String placeId;
+	private boolean isCoordsInit;
 	
+	public Location(String name, double latitude, double longitude, String placeId, boolean isCoordsInit) {
+		super();
+		this.name = name;
+		this.latitude = latitude;
+		this.longitude = longitude;
+		this.placeId = placeId;
+		this.isCoordsInit = isCoordsInit;
+	}
+	
+	/*
 	public Location(String name, double latitude, double longitude) {
 		super();
 		this.name = name;
 		this.latitude = latitude;
 		this.longitude = longitude;
-	}
+	}*/
 	
 	public String getName() {
 		return name;
@@ -30,6 +42,18 @@ public class Location {
 	}
 	public void setLongitude(double longitude) {
 		this.longitude = longitude;
+	}
+	
+	public String getPlaceId() {
+		return placeId;
+	}
+	
+	public void setPlaceId(String placeId) {
+		this.placeId = placeId;
+	}
+	
+	public boolean getIsCoordsInit() {
+		return isCoordsInit;
 	}
 
 	@Override
--- src/WeatherAppGUI/LocationField.java
+++ src/WeatherAppGUI/LocationField.java
@@ -2,12 +2,18 @@ package WeatherAppGUI;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
 
 import javax.swing.JComboBox;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 import javax.swing.text.JTextComponent;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import Util.APIQuery;
 import Util.Messages;
 
 public class LocationField extends JComboBox<Location> {
@@ -15,6 +21,13 @@ public class LocationField extends JComboBox<Location> {
 	private static final long serialVersionUID = -110676774074115141L;
 
 	private JTextComponent editor;
+	
+	private final String APIUrlPlace = "https://maps.googleapis.com/maps/api/place";
+	private final String Autocomplete = "autocomplete/json";
+	private final String Details = "details/json";
+    private final String APIKey = "AIzaSyDCWGTdf7HIrKAdSzIUX8B_Zb0RPn87Rn8";
+    
+    private boolean isLoaded;
 
 	public LocationField() {
 		super();
@@ -48,13 +61,50 @@ public class LocationField extends JComboBox<Location> {
 	
 	public void chooseCurrentLocation () {
 		GetLocation currLoc = new GetLocation();
-		this.addItem(new Location(currLoc.getCountry() + ", " + currLoc.getCity(), currLoc.getLat(), currLoc.getLon()));
+		this.addItem(new Location(currLoc.getCountry() + ", " + currLoc.getCity(), currLoc.getLat(), currLoc.getLon(), null, true));
 	}
 	
 	private void fetchLocations() {
+		var input = editor.getText();
+		isLoaded = false;
 		if(editor.getText().length() > 3) {
-			// TODO: Implement fetch
+			try {
+		        new APIQuery(APIUrlPlace)
+				.function(Autocomplete)
+				.addParam("input", input)
+	            .addParam("key", APIKey)
+				.exec(new APIQuery.APICallback() {
+					public void run(int responseCode, BufferedReader jsonResult, String errorMessage) {
+						try {
+							if(responseCode != APIQuery.OK) {
+								throw new RuntimeException(errorMessage);
+							}
+						
+							fillAutocompleteDataFromJson(jsonResult);
+							
+						} catch (Exception e) {
+							Messages.showError(e.getMessage());
+						}
+					}
+				});
+	        
+			} catch (Exception e) {
+				Messages.showError(e.getMessage());
+			}
 		}
+	}
+	
+	private void fillAutocompleteDataFromJson(BufferedReader jsonReader) throws Exception {
+		var json = (JSONObject) new JSONParser().parse(jsonReader);
+		
+		var predictions = (JSONArray) json.get("predictions");
+		for (int i = 0; i < predictions.size(); i++) {
+			var prediction = (JSONObject) predictions.get(i);
+            
+			var name = (String) prediction.get("description");
+            var placeId = (String) prediction.get("place_id");
+            this.addItem(new Location(name, 0, 0, null, false));
+        }
 	}
 	
 }
